a7d244e06648900121915a0647bd5b401cdf82d9
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-18 21:11:44 +0100

    hhh

--- Main.kt
+++ Main.kt
@@ -1,5 +1,12 @@
 fun main(){
   println("Hello Kotlin!")
+  val popcorn = 5    // There are 5 boxes of popcorn
+val hotdog = 7     // There are 7 hotdogs
+var customers = 10 // There are 10 customers in the queue
+
+// Some customers leave the queue
+customers = 8
+println(customers)
   val customers = 10
 println("There are $customers customers")
 

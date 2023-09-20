9c1ab5aa89ae010eec059d2cf384230686a7f7de
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-20 21:42:41 +0100

    crazt
    using if else to assign a variable name

--- FirstIJCommKt/src/main/kotlin/Main.kt
+++ FirstIJCommKt/src/main/kotlin/Main.kt
@@ -7,25 +7,27 @@ val name = "Peace"
 fun main() {
     println("Hello Kotlin")
 
-    //Variables in Kotlin
-    //mutable variables "var"
-    //read-only variables "val"
+//Variables in Kotlin
+//mutable variables "var"
+//read-only variables "val"
     var age: Int = 20
+    val greetingToPrint = if(greeting != null) greeting else "Hi"
 
-    //greeting = "Hello"
-    //if else statement, similar in python, java and C
-    //if (greeting != null){
-    //    println(greeting)
-    //} else {
-    //    println("Hi")
-    //}
+//    greeting = "Hello"
+//if else statement, similar in python, java and C
+//    if (greeting != null){
+//        println(greeting)
+//    } else {
+//        println("Hi")
+//    }
 
-    //when statement, similar to switch statement in Java
-    when (greeting) {
-        null -> println("Hiii")
-        else -> println(greeting)
-    }
+//    when statement, similar to switch statement in Java
+//    when (greeting) {
+//        null -> println("Hiii")
+//        else -> println(greeting)
+//    }
 
+    println(greetingToPrint)
     println(name)
 
 

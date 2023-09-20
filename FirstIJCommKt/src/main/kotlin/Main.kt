d4183d440f44ce763fee93fbd34b48943ccd0440
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-20 21:28:00 +0100

    not stoping

--- FirstIJCommKt/src/main/kotlin/Main.kt
+++ FirstIJCommKt/src/main/kotlin/Main.kt
@@ -1,9 +1,29 @@
+
+//top-level variables
+var greeting: String? = null
+//kotlin can automatically be detected by kotlin
+val name = "Peace"
+
 fun main() {
     println("Hello Kotlin")
+
     //Variables in Kotlin
     //mutable variables "var"
     //read-only variables "val"
-    val name: String = "Peace"
     var age: Int = 20
+
+    //greeting = "Hello"
+    //if else statement, similar in python, java and C
+    //if (greeting != null){
+    //    println(greeting)
+    //} else {
+    //    println("Hi")
+    //}
+
+    //when statement, similar to switch statement in Java
+
     println(name)
+
+
+
 }

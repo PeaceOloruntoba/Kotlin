ced117f168a6f8e13e6ef753ffeb72946ca552fe
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-20 21:12:55 +0100

    going

--- FirstIJCommKt/src/main/kotlin/Main.kt
+++ FirstIJCommKt/src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
 fun main() {
-    println("Hello World!")
-
+    println("Hello Kotlin")
+    //Variables in Kotlin
+    //mutable variables "var"
+    //read-only variables "val"
+    val name: String = "Peace"
+    var age: Int = 20
+    println(name)
 }

1bca59b2386f25db11ea511bbf45e553797aabf7
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-21 12:29:24 +0100

    fast

--- FirstIJCommKt/src/main/kotlin/Main.kt
+++ FirstIJCommKt/src/main/kotlin/Main.kt
@@ -1,34 +1,62 @@
+//
+////top-level variables
+//var greeting: String? = null
+////kotlin can automatically be detected by kotlin
+//val name = "Peace"
+//fun main() {
+//    println("Hello Kotlin")
+//
+////Variables in Kotlin
+////mutable variables "var"
+////read-only variables "val"
+//    var age: Int = 20
+////    greeting = "Hello"
+//    val greetingToPrint = if(greeting != null) greeting else "Hi"
+////if else statement, similar in python, java and C
+////    if (greeting != null){
+////        println(greeting)
+////    } else {
+////        println("Hi")
+////    }
+////    when statement, similar to switch statement in Java
+////    when (greeting) {
+////        null -> println("Hi")
+////        else -> println(greeting)
+////    }
+//    println(greetingToPrint)
+//    println(name)
+//
+////    println(greetingToPrint + name)
+////    println(greetingToPrint + " " + name)
+//
+//
+//}
 
-//top-level variables
-var greeting: String? = null
-//kotlin can automatically be detected by kotlin
-val name = "Peace"
+//BASIC FUNCTIONS
+
+//fun getGreeting(): String{
+//    return "Hello Kotlin"
+//}
+//single expression function
+//fun getGreeting() = "Hello Kotlin"
+//
+//fun sayHello() {
+//    println(getGreeting())
+//}
+//fun main() {
+//    println("Hello World")
+//    println(getGreeting())
+//    sayHello()
+//}
+
+//FUNCTION PARAMETERS
+
+fun sayHello(itemToGreet:String){
+    val msg = "Hello " + itemToGreet
+    println(msg)
+}
+
 fun main() {
-    println("Hello Kotlin")
-
-//Variables in Kotlin
-//mutable variables "var"
-//read-only variables "val"
-    var age: Int = 20
-    val greetingToPrint = if(greeting != null) greeting else "Hi"
-//    greeting = "Hello"
-//if else statement, similar in python, java and C
-//    if (greeting != null){
-//        println(greeting)
-//    } else {
-//        println("Hi")
-//    }
-//    when statement, similar to switch statement in Java
-//    when (greeting) {
-//        null -> println("Hiii")
-//        else -> println(greeting)
-//    }
-    println(greetingToPrint)
-    println(name)
-
-    println(greetingToPrint + name)
-    println(greetingToPrint + " " + name)
-
-
-
+    sayHello(itemToGreet="Kotlin")
+    sayHello(itemToGreet="Peace")
 }

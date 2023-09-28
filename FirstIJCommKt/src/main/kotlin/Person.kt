ece586542a5eff9c2079defd40b2754c6a0b757f
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-28 02:10:54 +0100

    vnjh

--- FirstIJCommKt/src/main/kotlin/Person.kt
+++ FirstIJCommKt/src/main/kotlin/Person.kt
@@ -24,8 +24,10 @@
 //VISIBILITY OF CLASSES
 // class are public by default, but you can also specify it by using the public keywords, if a class is public then it can be accessed from anywhere in the project
 // class also can be defined as internal, that is, it will only be visible in the dir and in the module where it is located, you also define this by using the internal keyword
-// class can also be private that is it wil only be available within the class and it's sub classes
+// class can also be private that is it wil only be available within the class, and it's sub
 class Person(val firstName: String = "Peace", val lastName: String = "Oloruntoba"){
+
+    //VISIBILITY OF PROPERTIES, just like classes also, properties and methods too can be made visible or invincible, using some set of keywords
     var nickName: String? = null
         set(value) {
             field = value

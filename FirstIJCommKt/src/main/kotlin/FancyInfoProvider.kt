2a328d51b4f8862f5128a02d8d12a0e52b63a9c5
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-30 03:46:52 +0100

    jh

--- FirstIJCommKt/src/main/kotlin/FancyInfoProvider.kt
+++ FirstIJCommKt/src/main/kotlin/FancyInfoProvider.kt
@@ -1,6 +1,8 @@
 
 class FancyInfoProvider : BasicInfoProvider(){
 
+    override val sessionIdPrefix: String
+        get() = "Fancy Session"
 
     override val providerInfo: String
         get() = "Fancy Info Provider"

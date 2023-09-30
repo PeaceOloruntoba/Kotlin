66beed1a07a05f640417d0ee56f216543ac56057
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-30 03:43:14 +0100

    hgfhj

--- FirstIJCommKt/src/main/kotlin/FancyInfoProvider.kt
+++ FirstIJCommKt/src/main/kotlin/FancyInfoProvider.kt
@@ -2,4 +2,9 @@
 class FancyInfoProvider : BasicInfoProvider(){
     override val providerInfo: String
         get() = "Fancy Info Provider"
+
+    override fun printInfo(person: Person) {
+        super.printInfo(person)
+        println("Fancy Info")
+    }
 }

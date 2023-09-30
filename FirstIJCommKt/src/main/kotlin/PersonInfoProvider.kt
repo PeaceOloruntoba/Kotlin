68ea7cb50c7ba9d9bcc619c3182f78a4e332c442
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2023-09-30 03:32:33 +0100

    inheritance

--- FirstIJCommKt/src/main/kotlin/PersonInfoProvider.kt
+++ FirstIJCommKt/src/main/kotlin/PersonInfoProvider.kt
@@ -40,6 +40,59 @@
 //
 //TYPE-CHECKING AND TYPE-CASTING IN KOTLIN
 
+//interface PersonInfoProvider{
+//    val providerInfo : String
+//
+//    fun printInfo(person: Person){
+//        println(providerInfo)
+//        person.printInfo()
+//    }
+//}
+//
+//interface SessionInfoProvider{
+//    fun getSessionId(): String
+//}
+//
+//
+//class BasicInfoProvider : PersonInfoProvider, SessionInfoProvider{
+////    override fun printInfo(person: Person) {
+////        println("basicInfoProvider")
+////        person.printInfo()
+////    }
+//    override val providerInfo: String
+//        get() = "BasicInfoProvider"
+//
+//    override fun printInfo(person: Person) {
+//        super.printInfo(person)
+//        println("Additional Junks")
+//    }
+//
+//    override fun getSessionId(): String {
+//        return "Session"
+//    }
+//}
+//
+//fun main() {
+//    val provider = BasicInfoProvider()
+//
+//    provider.printInfo(Person())
+//    provider.getSessionId()
+//
+//    checkTypes(provider)
+//}
+//
+//fun checkTypes(infoProvider: PersonInfoProvider){
+//    if (infoProvider is SessionInfoProvider) {
+//        println("is a session info provider")
+//    } else {
+//        println("not a session info provider")
+//        //typecasting
+//        (infoProvider as SessionInfoProvider).getSessionId()
+//    }
+//}
+
+//INHERITANCE IN KOTLIN
+
 interface PersonInfoProvider{
     val providerInfo : String
 
@@ -55,7 +108,7 @@ interface SessionInfoProvider{
 
 
 class BasicInfoProvider : PersonInfoProvider, SessionInfoProvider{
-//    override fun printInfo(person: Person) {
+    //    override fun printInfo(person: Person) {
 //        println("basicInfoProvider")
 //        person.printInfo()
 //    }
